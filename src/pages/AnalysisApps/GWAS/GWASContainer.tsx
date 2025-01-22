e99efaead039f8e174c817e46f0005b647669b94
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-01-22 11:05:42 -0600

    spike(PortDemo): removed missing prop breaking compilation

--- src/pages/AnalysisApps/GWAS/GWASContainer.tsx
+++ src/pages/AnalysisApps/GWAS/GWASContainer.tsx
@@ -109,7 +109,7 @@ const GWASContainer = () => {
       <div>
         <div className="flex justify-between pb-4">
           <h1 className="text-3xl pb-5 font-medium">Gen3 GWAS</h1>
-          <TeamProjectHeader />
+          <TeamProjectHeader isEditable={false} />
         </div>
       </div>
       <p className="pb-8 text-sm">
