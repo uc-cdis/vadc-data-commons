12aa70f0c1e052c52daa6765662d66d8e8eedc1f
craigrbarnes <craig.barnes@intelligentlight.com>
2025-01-31 10:33:35 -0600

    add teamProjectHooks.ts

---
+++ src/lib/AnalysisApps/SharedUtils/TeamProject/Utils/teamProjectHooks.ts
@@ -0,0 +1,53 @@
+import { useGetAuthzMappingsQuery } from '@gen3/core';
+
+interface TeamProject {
+  teamName: string;
+}
+
+interface Teams {
+  teams: Array<TeamProject>;
+}
+
+interface UseTeamProjectsResult {
+  teams: Teams;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+}
+
+/**
+ * A hook that retrieves and processes team project data by querying authorization mappings.
+ * It filters the retrieved data to only include mappings associated with specific services
+ * under the `/gwas_projects/` path and formats them into a list of team projects.
+ *
+ * @returns {UseTeamProjectsResult} An object containing the list of team projects,
+ * and flags indicating the loading, success, or error state of the data retrieval process.
+ */
+export const useTeamProjects = (): UseTeamProjectsResult => {
+  const {
+    data: authorizationMappings,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useGetAuthzMappingsQuery();
+
+  let teamProjectList: Teams = { teams: [] };
+
+  if (isSuccess && authorizationMappings) {
+    const entries = Object.entries(authorizationMappings);
+    const teams = entries
+      .filter(
+        ([key, value]) =>
+          key.startsWith('/gwas_projects/') &&
+          Array.isArray(value) &&
+          value.some(
+            (e) => e.service === 'atlas-argo-wrapper-and-cohort-middleware',
+          ),
+      )
+      .map(([key]) => ({ teamName: key }));
+
+    teamProjectList = { teams };
+  }
+
+  return { teams: teamProjectList, isLoading, isSuccess, isError };
+};
