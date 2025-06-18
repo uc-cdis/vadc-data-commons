482b6384120fca26e0977cc2137d70a801c3edd3
Shawn O'Connor <shawnoconnor@uchicago.edu>
2025-06-18 13:14:29 -0500

    CADC-1666: Create Input Details Page

--- src/lib/AnalysisApps/SharedUtils/Endpoints.ts
+++ src/lib/AnalysisApps/SharedUtils/Endpoints.ts
@@ -1,4 +1,4 @@
 export const CohortsEndpoint = 'api/cohorts';
 export const TeamProjectsEndpoint = 'api/teamprojects';
-//export const gwasWorkflowPath = 'ga4gh/wes/v2/';
+export const gwasWorkflowPath = 'ga4gh/wes/v2/';
 export const GwasWorkflowEndpoint = 'ga4gh/wes/v2/workflows';
